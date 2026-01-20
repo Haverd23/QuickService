0fe18e252ee3eaef0b584d91f723ae1581435e93
João <GabrielAlvees616@gmail.com>
2026-01-20 00:43:46 -0300

    Criado a camada de Data do Contexto Users

---
+++ QuickService/src/User/QUS.Users.Data/Repository/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using QUS.Users.Domain.Interfaces;
+using QUS.Users.Domain.Models;
+
+namespace QUS.Users.Data.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserDbContext _context;
+        public UserRepository(UserDbContext context)
+        {
+            _context = context;
+        }
+        public Task Add(User user)
+        {
+           _context.Users.Add(user);
+           _context.SaveChanges();
+           return Task.CompletedTask;
+        }
+    }
+}
---
+++ QuickService/src/User/QUS.Users.Data/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QUS.Users.Domain.Models;
+namespace QUS.Users.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
