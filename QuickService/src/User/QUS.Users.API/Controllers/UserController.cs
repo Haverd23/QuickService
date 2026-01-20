8d6f0d6eb6d63988b2ee14cd2baffdb9f9141c8a
João <GabrielAlvees616@gmail.com>
2026-01-20 02:28:01 -0300

    Criado a camada de API do contexto de User

---
+++ QuickService/src/User/QUS.Users.API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using QUS.Users.API.DTOs;
+using QUS.Users.Application;
+
+namespace QUS.Users.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] UserDTO userDto)
+        {
+             _userService.CreateUser(userDto.Name,userDto.Email,userDto.Phone);
+            return Created();
+        }
+    }
+}
