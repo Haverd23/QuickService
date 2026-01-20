dcf43f1cc23dc050808506b72571d2796fcaa09c
João <GabrielAlvees616@gmail.com>
2026-01-19 23:42:27 -0300

    Criado Contexto de User e o Aggregate root junto com sua interface repository

---
+++ QuickService/src/User/QUS.User.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using QUS.Users.Domain.Models;
+namespace QUS.Users.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task Add(User user);
+    }
+}
---
+++ QuickService/src/User/QUS.User.Domain/Models/User.cs
@@ -0,0 +1,37 @@
+﻿namespace QUS.Users.Domain.Models
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+
+        public User(string name, string email, string phone)
+        {
+            IsValid(name, email, phone);
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+        private void IsValid(string name, string email, string phone)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Nome não pode ser vazio");
+            }
+            if(name.Length < 3)
+            {
+                throw new ArgumentException("Nome deve ter pelo menos 3 caracteres");
+            }
+            if(!email.Contains("@"))
+            {
+                throw new ArgumentException("Email inválido");
+            }
+            if(phone.Length < 10)
+            {
+                throw new ArgumentException("Telefone inválido");
+            }
+
+        }
+    }
+}
