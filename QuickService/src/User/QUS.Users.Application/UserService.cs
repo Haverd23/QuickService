a8a2cbf308782bf333c26310c320f96df427306b
João <GabrielAlvees616@gmail.com>
2026-01-20 01:16:42 -0300

    Criado Camada de Application do Contexto User

---
+++ QuickService/src/User/QUS.Users.Application/UserService.cs
@@ -0,0 +1,20 @@
+﻿using QUS.Users.Domain.Interfaces;
+using QUS.Users.Domain.Models;
+
+namespace QUS.Users.Application
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public void CreateUser(string name, string email, string phone)
+        {
+            var user =  new User(name, email, phone);
+
+            _userRepository.Add(user);
+        }
+    }
+}
