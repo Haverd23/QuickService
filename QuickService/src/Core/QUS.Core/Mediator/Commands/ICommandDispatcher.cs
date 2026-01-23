cd7ef397c63e7b0b2250371e3bb2d5f680784b32
João <GabrielAlvees616@gmail.com>
2026-01-23 13:22:06 -0300

    Criado interfaces de Commands na camada Core

---
+++ QuickService/src/Core/QUS.Core/Mediator/Commands/ICommandDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUS.Core.Mediator.Commands
+{
+    public interface ICommandDispatcher
+    {
+        Task<TResult> DispatchAsync<TCommand, TResult>(TCommand command)
+            where TCommand : ICommand<TResult>;
+    }
+}
