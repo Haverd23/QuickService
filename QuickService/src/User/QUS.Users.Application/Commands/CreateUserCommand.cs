8ef46a6058cc85f5555245eef0bedcdd28c24cad
João <GabrielAlvees616@gmail.com>
2026-01-23 13:35:29 -0300

    Criado Commands de criação de User

---
+++ QuickService/src/User/QUS.Users.Application/Commands/CreateUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using QUS.Core.Mediator.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUS.Users.Application.Commands
+{
+    public class CreateUserCommand : ICommand<Guid>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public CreateUserCommand(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
