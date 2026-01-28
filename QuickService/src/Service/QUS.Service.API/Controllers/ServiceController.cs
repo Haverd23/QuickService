227f782ae9cb45c819711b09477c5558ef3f1c16
João <GabrielAlvees616@gmail.com>
2026-01-28 13:00:37 -0300

    Criado a camada de API do contexto de Service

---
+++ QuickService/src/Service/QUS.Service.API/Controllers/ServiceController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QUS.Core.Mediator.Commands;
+using QUS.Service.API.DTOs;
+using QUS.Service.Application.Commands;
+using System.Security.Claims;
+
+namespace QUS.Service.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        public ServiceController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateService([FromBody] ServiceDTO serviceDTO )
+        {
+            var idString = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            if (!Guid.TryParse(idString, out Guid userId))
+            {
+                return Unauthorized("Id do usuário inválido no token.");
+            }
+            var command = new CreateServiceCommand(
+                serviceDTO.Title,
+                serviceDTO.Description,
+                serviceDTO.Price,
+                userId,
+                serviceDTO.Category
+            );
+            var serviceId = await _commandDispatcher.DispatchAsync<CreateServiceCommand, Guid>(command);
+            return CreatedAtAction(nameof(CreateService), new { id = serviceId }, null);
+        }
+    }
+}
---
+++ QuickService/src/Service/QUS.Service.API/Extensions/DependencyInjectionConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QUS.Core.Mediator.Commands;
+using QUS.Service.Application.Commands;
+using QUS.Service.Application.Service;
+using QUS.Services.Application.CommandsHandlers;
+using QUS.Services.Data;
+using QUS.Services.Data.Repository;
+using QUS.Services.Domain.Interfaces;
+
+namespace QUS.Service.API.Extensions
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection AddDependencyInjection(this
+            IServiceCollection services, IConfiguration configuration)
+        {
+            // Data
+            services.AddScoped<IServiceRepository, ServiceRepository>();
+            services.AddDbContext<ServiceDbContext>(options =>
+          options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Application
+            services.AddScoped<ICommandHandler<CreateServiceCommand, Guid>, CreateServiceCommandHandler>();
+            services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+
+
+            return services;
+        }
+    }
+}
