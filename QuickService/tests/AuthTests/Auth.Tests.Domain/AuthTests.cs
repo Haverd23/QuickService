994c03dfb14c02b836e9d9cd8306da3631a3ff7d
João <GabrielAlvees616@gmail.com>
2026-01-31 12:09:46 -0300

    Criado testes unitários de Auth

---
+++ QuickService/tests/AuthTests/Auth.Tests.Domain/AuthTests.cs
@@ -0,0 +1,67 @@
+using QUS.Auth.Domain.Models;
+
+namespace Auth.Tests.Domain
+{
+    public class AuthTests
+    {
+        [Fact(DisplayName = "Criar User com dados válidos")]
+        public void Construtor_QuandoDadosValidos_DeveInstanciar()
+        {
+            // Arrange
+            var email = "teste@gmail.com";
+            var password = "Senha123";
+
+            // Act
+            var user = new User(email, password);
+
+            // Assert
+            Assert.NotNull(user);
+            Assert.Equal(email, user.Email.Entrada);
+            Assert.Equal(password, user.Password);
+
+
+        }
+        [Fact(DisplayName = "Criar User com senha inválida lança Exceção")]
+        public void Construtor_QuandoSenhaInvalida_DeveLancarExcecao()
+        {
+            // Arrange
+            var email = "teste@gmail.com";
+            var password = "123"; // Senha inválida
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => new User(email, password));
+        }
+        [Fact(DisplayName = "Criar User com email inválido lança Exceção")]
+        public void Construtor_QuandoEmailInvalido_DeveLancarExcecao()
+        {
+            // Arrange
+            var email = "teste.com"; // Email inválido
+            var password = "Senha123";
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => new User(email, password));
+        }
+        [Fact(DisplayName = "Criar User com senha sem número lança Exceção")]
+        public void Construtor_QuandoSenhaSemNumero_DeveLancarExcecao()
+        {
+            // Arrange
+            var email = "teste@gmail.com";
+            var password = "Senha"; // Senha sem número
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => new User(email, password));
+
+
+        }
+        [Fact(DisplayName = "Criar User com senha sem letra maiúscula lança Exceção")]
+        public void Construtor_QuandoSenhaSemLetraMaiuscula_DeveLancarExcecao()
+        {
+            // Arrange
+            var email = "teste@gmail.com";
+            var password = "senha123"; // Senha sem letra maiúscula
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => new User(email, password));
+
+        }
+    }
+}
