5e0eaa294650641dcc6c47c8d85fd69acf7ff450
João <GabrielAlvees616@gmail.com>
2026-02-03 02:03:02 -0300

    Criado Interfaces de eventos de domínio

---
+++ api/QuickService/src/Core/QUS.Core/DomainObjects/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUS.Core.DomainObjects
+{
+    public interface IDomainEvent 
+    {
+        public DateTime OccurredOn { get; }
+    }
+}
