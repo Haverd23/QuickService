4ad20884555c70bc65b593625af1bae28af577f3
João <GabrielAlvees616@gmail.com>
2026-02-05 15:33:31 -0300

    Criado Miggrations de Auth

---
+++ api/QuickService/src/Auth/QUS.Auth.Data/Migrations/20260205160544_First-AuthMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QUS.Auth.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstAuthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersAuth",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email_Entrada = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(1024)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersAuth", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersAuth");
+        }
+    }
+}
