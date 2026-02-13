d94d2d83c1b57edc4e9ed4a256c0aa98fc43679f
João <GabrielAlvees616@gmail.com>
2026-02-13 12:49:12 -0300

    Criado Página Login e Registro

--- web/QuickService/src/app/app.component.ts
+++ web/QuickService/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from "./features/home/pages/home-page/home-page.component";
 import { HeaderComponent } from "./core/components/header/header.component";
 import { FooterComponent } from './core/components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- web/QuickService/src/app/app.routes.ts
+++ web/QuickService/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
+import { AuthPageComponent } from './features/auth/pages/auth-pages/auth-pages.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./features/home/home.routes').then(m => m.HOME_ROUTES)
+  },
+  {
+    path: 'login',
+    component: AuthPageComponent,
+    data: { type: 'login' }
+  },
+  {
+    path: 'register',
+    component: AuthPageComponent,
+    data: { type: 'register' }
   }
 ];
