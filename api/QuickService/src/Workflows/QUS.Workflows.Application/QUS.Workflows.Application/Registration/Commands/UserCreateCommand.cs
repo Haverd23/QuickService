46c8fe812ec71f0e18ea53375dcf874aa2277eed
João <GabrielAlvees616@gmail.com>
2026-02-23 23:57:49 -0300

    Adiciona comando e evento de criação de User no Workflow para orquestração via mensageria

---
+++ api/QuickService/src/Workflows/QUS.Workflows.Application/QUS.Workflows.Application/Registration/Commands/UserCreateCommand.cs
@@ -0,0 +1,26 @@
+﻿using QUS.Core.Mediator.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QUS.Workflows.Application.Registration.Commands
+{
+    public class UserCreateCommand : ICommand<Guid>
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string Phone { get; }
+        public Guid AuthId { get; }
+
+        public UserCreateCommand(string name, string email, string phone, Guid authId)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            AuthId = authId;
+        }
+    }
+}
---
+++ api/QuickService/src/Workflows/QUS.Workflows.Application/QUS.Workflows.Application/Registration/Events/UserCreateEvent.cs
@@ -0,0 +1,25 @@
+﻿using QUS.Workflows.Application.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUS.Workflows.Application.Registration.Events
+{
+    public class UserCreateEvent : ApplicationEvent
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string Phone { get; }
+        public Guid AuthId { get; }
+
+        public UserCreateEvent(string name, string email, string phone, Guid authId)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            AuthId = authId;
+        }
+    }
+}
